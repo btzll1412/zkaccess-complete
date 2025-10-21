ee3896e73a411ff6cb414bd83f9a6e6e3b21ed05
btzll1412 <145881682+btzll1412@users.noreply.github.com>
2025-10-20 23:58:22 -0400

    Update config_flow.py

--- custom_components/zkaccess_complete/config_flow.py
+++ custom_components/zkaccess_complete/config_flow.py
@@ -8,7 +8,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
 
@@ -16,42 +16,10 @@ from .const import (
     DOMAIN,
     CONF_PANEL_NAME,
     CONF_PASSWORD,
-    CONF_DOORS,
     DEFAULT_PORT,
 )
-from .api.c3_client import C3Client
 
 _LOGGER = logging.getLogger(__name__)
-
-
-async def validate_connection(
-    hass: HomeAssistant, data: dict[str, Any]
-) -> dict[str, Any]:
-    """Validate the user input allows us to connect."""
-    
-    client = C3Client(
-        ip=data[CONF_IP_ADDRESS],
-        port=data.get(CONF_PORT, DEFAULT_PORT),
-        password=data.get(CONF_PASSWORD, ""),
-    )
-    
-    # Test connection
-    if not await hass.async_add_executor_job(client.connect):
-        raise ConnectionError("Cannot connect to panel")
-    
-    # Get panel info
-    info = await hass.async_add_executor_job(client.get_device_info)
-    
-    # Disconnect
-    await hass.async_add_executor_job(client.disconnect)
-    
-    return {
-        "title": data[CONF_PANEL_NAME],
-        "serial_number": info.get("serial_number", "Unknown"),
-        "model": info.get("model", "C3-400"),
-        "firmware": info.get("firmware", "Unknown"),
-        "door_count": info.get("door_count", 4),
-    }
 
 
 class ZKAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -67,21 +35,21 @@ class ZKAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             try:
-                info = await validate_connection(self.hass, user_input)
-            except ConnectionError:
-                errors["base"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
+                # Basic validation - just check if IP is provided
+                ip = user_input[CONF_IP_ADDRESS]
+                
                 # Create unique ID based on IP
-                await self.async_set_unique_id(user_input[CONF_IP_ADDRESS])
+                await self.async_set_unique_id(ip)
                 self._abort_if_unique_id_configured()
                 
                 return self.async_create_entry(
-                    title=info["title"],
+                    title=user_input[CONF_PANEL_NAME],
                     data=user_input,
                 )
+                
+            except Exception as err:
+                _LOGGER.exception("Unexpected exception: %s", err)
+                errors["base"] = "unknown"
 
         # Show form
         data_schema = vol.Schema(
@@ -89,7 +57,7 @@ class ZKAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_PANEL_NAME, default="Access Panel"): str,
                 vol.Required(CONF_IP_ADDRESS): str,
                 vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-                vol.Optional(CONF_PASSWORD): str,
+                vol.Optional(CONF_PASSWORD, default=""): str,
             }
         )
 
@@ -97,13 +65,9 @@ class ZKAccessConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=data_schema,
             errors=errors,
-            description_placeholders={
-                "docs_url": "https://github.com/btzll1412/zkaccess-complete"
-            },
         )
 
     @staticmethod
-    @callback
     def async_get_options_flow(
         config_entry: config_entries.ConfigEntry,
     ) -> ZKAccessOptionsFlowHandler:
